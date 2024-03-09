0c03dd631356c74c49095dcb3f505830d3560013
JBLDSKY <JBLDSKY@gmail.com>
2024-03-09 02:59:56 +0100

    remove compiled files from repository

--- frontend/src/scripts/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const game_1 = require("./game");
-window.onload = () => {
-    console.log("starting");
-    const game = new game_1.SpaceInvadersGame('gameCanvas');
-    game.start();
-};
