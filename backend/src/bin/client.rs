aead0f5281826bdf67d9945020856dff85509726
straws11 <dswarts11@gmail.com>
2024-04-12 16:06:58 +0200

    Add DatabaseClient class, test methods

--- backend/src/bin/client.rs
+++ backend/src/bin/client.rs
@@ -6,8 +6,11 @@ use postgres_openssl::MakeTlsConnector;
 
 use std::error;
 
-fn main() -> Result<(), Box<dyn error::Error>> {
-    let builder = SslConnector::builder(SslMethod::tls())?;
+pub mod db;
+use crate::db::db::DatabaseClient;
+
+fn main() {
+    /*let builder = SslConnector::builder(SslMethod::tls())?;
 
     let connector = MakeTlsConnector::new(builder.build());
 
@@ -18,7 +21,13 @@ fn main() -> Result<(), Box<dyn error::Error>> {
 
     let _tables = get_table_names(&mut client)?;
 
-    Ok(())
+    Ok(())*/
+
+    let mut db_client = DatabaseClient::new().unwrap();
+    for row in db_client.head().unwrap() {
+        let uname: &str = row.get("username");
+        println!("{}", uname);
+    }
 }
 
 fn get_table_names(client: &mut Client) -> Result<Vec<Row>, postgres::Error> {
