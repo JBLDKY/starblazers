ce85c6e9a0399cee2c299fd22717aa3b3a45ecb9
JBLDSKY <JBLDSKY@gmail.com>
2024-06-13 14:30:04 +0200

    Default for mws

--- backend/src/lib/websocket.rs
+++ backend/src/lib/websocket.rs
@@ -17,6 +17,12 @@ pub struct MyWebSocket {
     hb: Instant,
 }
 
+impl Default for MyWebSocket {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl MyWebSocket {
     pub fn new() -> Self {
         Self { hb: Instant::now() }
