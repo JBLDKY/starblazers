83af2ee016534d0d58227532e830f47561f21289
JBLDSKY <JBLDSKY@gmail.com>
2024-06-14 18:59:41 +0200

    push to ringbuffer

--- backend/src/lib/ringbuffer.rs
+++ backend/src/lib/ringbuffer.rs
@@ -2,6 +2,7 @@ struct RingBuffer<T, const S: usize> {
     buffer: [Option<T>; S],
     start: usize,
     end: usize,
+    full: bool,
 }
 
 impl<T, const S: usize> RingBuffer<T, S> {
@@ -12,6 +13,7 @@ impl<T, const S: usize> RingBuffer<T, S> {
             buffer: [(); S].map(|_| None),
             start: 0,
             end: 0,
+            full: false,
         }
     }
 
@@ -19,8 +21,62 @@ impl<T, const S: usize> RingBuffer<T, S> {
         self.buffer[self.end] = Some(item);
         self.end = (self.end + 1) % S;
 
-        if self.end == self.start {
-            self.start = (self.start + 1) % S
+        if self.full {
+            self.start = (self.start + 1) % S;
         }
+
+        if self.end == self.start {
+            self.full = true;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ring_buffer_new() {
+        let ring_buffer: RingBuffer<usize, 5> = RingBuffer::new();
+        assert_eq!(ring_buffer.start, 0);
+        assert_eq!(ring_buffer.end, 0);
+        for elem in ring_buffer.buffer.iter() {
+            assert!(elem.is_none());
+        }
+    }
+
+    #[test]
+    fn test_ring_buffer_push() {
+        let mut ring_buffer: RingBuffer<usize, 3> = RingBuffer::new();
+        ring_buffer.push(1);
+        assert_eq!(ring_buffer.buffer[0], Some(1));
+        assert_eq!(ring_buffer.end, 1);
+        assert_eq!(ring_buffer.start, 0);
+
+        ring_buffer.push(2);
+        assert_eq!(ring_buffer.buffer[1], Some(2));
+        assert_eq!(ring_buffer.end, 2);
+        assert_eq!(ring_buffer.start, 0);
+
+        ring_buffer.push(3);
+        assert_eq!(ring_buffer.buffer[2], Some(3));
+        assert_eq!(ring_buffer.end, 0);
+        assert_eq!(ring_buffer.start, 0);
+
+        // Pushing another element should overwrite the oldest element
+        ring_buffer.push(4);
+        assert_eq!(ring_buffer.buffer[0], Some(4));
+        assert_eq!(ring_buffer.end, 1);
+        assert_eq!(ring_buffer.start, 1);
+
+        ring_buffer.push(5);
+        assert_eq!(ring_buffer.buffer[1], Some(5));
+        assert_eq!(ring_buffer.end, 2);
+        assert_eq!(ring_buffer.start, 2);
+
+        ring_buffer.push(6);
+        assert_eq!(ring_buffer.buffer[2], Some(6));
+        assert_eq!(ring_buffer.end, 0);
+        assert_eq!(ring_buffer.start, 0);
     }
 }
