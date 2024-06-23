4a8130916546d19106f7b89cbda3d5de04131758
JBLDSKY <JBLDSKY@gmail.com>
2024-06-23 18:33:17 +0200

    rename lobby_manager module

--- backend/src/lib/multiplayer/actors/mod.rs
+++ backend/src/lib/multiplayer/actors/mod.rs
@@ -1,7 +1,7 @@
-pub mod lobby_manager;
+pub mod lobby;
 pub mod user_state_manager;
 pub mod wslobbysession;
 
-pub use lobby_manager::LobbyManager;
+pub use lobby::LobbyManager;
 pub use user_state_manager::UserStateManager;
 pub use wslobbysession::WsLobbySession;
