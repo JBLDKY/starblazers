815f1eed0915233d3e1c653d669aac23a0b5defa
JBLDSKY <JBLDSKY@gmail.com>
2024-07-05 15:36:21 +0200

    cleanup before starting

--- backend/src/bin/client.rs
+++ backend/src/bin/client.rs
@@ -1,23 +1,4 @@
-#![allow(dead_code)]
-use postgres::{Client, Row};
-// use service::cli::handle_cli_input;
-
 fn main() {
-    std::env::set_var("RUST_LOG", "info");
+    std::env::set_var("RUST_LOG", "debug");
     pretty_env_logger::init();
-    log::info!("Starting starburst client");
-
-    // handle_cli_input();
-}
-
-fn get_table_names(client: &mut Client) -> Result<Vec<Row>, postgres::Error> {
-    client.query(
-        "
-    SELECT table_name
-    FROM information_schema.tables
-    WHERE table_type = 'BASE TABLE'
-    AND table_schema = 'public';
-        ",
-        &[],
-    )
 }
